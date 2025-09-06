6557744cdcc3c30b083fa52932e2598739fcdb65
Willard-nt <willardnixoniii@gmail.com>
2025-09-06 18:45:07 -0500

    set parameters for stage operators in dag
    adapted for airflow version 3

--- dags/final_project.py
+++ dags/final_project.py
@@ -28,11 +28,54 @@ def final_project():
 
     stage_events_to_redshift = StageToRedshiftOperator(
         task_id='Stage_events',
+        redshift_conn_id='redshift',
+        create_staging_events_table="""
+            DROP table IF EXISTS public.staging_events;
+                CREATE TABLE public.staging_events (
+	            artist varchar(256),
+	            auth varchar(256),
+	            firstname varchar(256),
+	            gender varchar(256),
+	            iteminsession int4,
+	            lastname varchar(256),
+	            length numeric(18,0),
+	            "level" varchar(256),
+	            location varchar(256),
+	            "method" varchar(256),
+	            page varchar(256),
+	            registration numeric(18,0),
+	            sessionid int4,
+	            song varchar(256),
+	            status int4,
+	            ts int8,
+	            useragent varchar(256),
+	            userid int4
+            );""",
+                create_staging_songs_table=None
+
     )
 
     stage_songs_to_redshift = StageToRedshiftOperator(
         task_id='Stage_songs',
-    )
+        redshift_conn_id='redshift',
+        create_staging_events_table=None,
+        create_staging_songs_table="""
+            DROP table IF EXISTS public.songs;
+                CREATE TABLE public.staging_songs (
+	            num_songs int4,
+	            artist_id varchar(256),
+	            artist_name varchar(512),
+	            artist_latitude numeric(18,0),
+	            artist_longitude numeric(18,0),
+	            artist_location varchar(512),
+	            song_id varchar(256),
+	            title varchar(512),
+	            duration numeric(18,0),
+	            "year" int4
+            );"""
+                
+
+)
 
     load_songplays_table = LoadFactOperator(
         task_id='Load_songplays_fact_table',
@@ -58,4 +101,7 @@ def final_project():
         task_id='Run_data_quality_checks',
     )
 
+    start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
+
 final_project_dag = final_project()
+
