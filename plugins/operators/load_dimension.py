a5f92e0fea9b14f93a05543434c6896b5b624ca7
Willard-nt <willardnixoniii@gmail.com>
2025-09-19 11:35:01 -0500

    operator compatible with different insert modes

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -1,22 +1,39 @@
-from airflow.hooks.postgres_hook import PostgresHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.models import BaseOperator
-from airflow.utils.decorators import apply_defaults
 
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
 
-    @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
+                 redshift_conn_id: str,
+                 sql: str,
+                 table:str,
+                 mode: str = 'overwrite',
+                 **kwargs):
 
-        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        super(LoadDimensionOperator, self).__init__(**kwargs)
+        self.redshift_conn_id=redshift_conn_id
+        self.sql = sql
+        self.table = table
+        self.mode = mode.lower()
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        if self.mode not in ['overwrite', 'append']:
+            raise ValueError(f"Invalid mode '{self.mode}'. Use 'overwrite' or 'append'.")
+
+
+        self.log.info("Connecting to Redshift database")
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        
+        if self.mode == 'overwrite':
+            self.log.info(f"Truncating {self.table} table; overwriting data")
+            redshift.run(f"TRUNCATE TABLE {self.table};")
+            redshift.run(self.sql)
+
+        else:
+            self.log.info(f"Appending data to {self.table} table")
+            redshift.run(self.sql)
+
+        self.log.info("Loaded data to dimension tables succesfully")
