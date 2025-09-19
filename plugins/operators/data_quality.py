09c97d3958d014bb10b09165839bc2fd0cdb7e2f
Willard-nt <willardnixoniii@gmail.com>
2025-09-19 11:31:41 -0500

    allow data quality operator to check for errors

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -1,22 +1,28 @@
-from airflow.hooks.postgres_hook import PostgresHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.models import BaseOperator
-from airflow.utils.decorators import apply_defaults
-
+from airflow.exceptions import AirflowException
 class DataQualityOperator(BaseOperator):
 
     ui_color = '#89DA59'
 
-    @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
+                 redshift_conn_id: str,
+                 tables: '',
+                 **kwargs):
 
-        super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        super(DataQualityOperator, self).__init__(**kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.tables = tables or []
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        self.log.info('Connecting to Redshift database')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        for table in self.tables:
+            self.log.info(f"Running data quality check on table: {table}")
+            records = redshift.get_records(f"SELECT COUNT(*) FROM {table};")
+
+            if not records or records[0][0] < 1:
+                raise AirflowException(f"Data quality check failed: {table} returned no rows.")
+            else:
+                self.log.info(f"Data quality check passed for {table}: {records[0][0]} records found.")
